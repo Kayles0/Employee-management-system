11411edcde46afe4aa8d54d9016578743ab01a19
Aleksey <aleksejiwanow@bk.ru>
2025-03-03 21:57:31 +0300

    Add annotation
    EqualsAndHashCode

--- src/main/java/com/kayles/employee_management_system/entity/Group.java
+++ src/main/java/com/kayles/employee_management_system/entity/Group.java
@@ -3,10 +3,12 @@ package com.kayles.employee_management_system.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
 @NoArgsConstructor
